db3a591aa5ab19ad599ffb6bd9bcbdc747a3f45c
Roberto Boscolo Figueredo <33357673+betosan83@users.noreply.github.com>
2020-10-28 21:31:23 -0300

    Initial commit.

---
+++ src/main/java/rbf/spring/spring5cookingapp/Spring5CookingAppApplication.java
@@ -0,0 +1,13 @@
+package rbf.spring.spring5cookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5CookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5CookingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rbf/spring/spring5cookingapp/Spring5CookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rbf.spring.spring5cookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5CookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
